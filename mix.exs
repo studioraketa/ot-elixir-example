0cf2989d4b1b95e3321df7bc943185e516509b09
Vestimir Markov <me@vestimir.com>
2024-08-23 08:44:53 +0300

    Add osmon for Elixir telemetry

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule RollDice.MixProject do
   def application do
     [
       mod: {RollDice.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
